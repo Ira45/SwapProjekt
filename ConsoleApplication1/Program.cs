6710dcfd9d53371065a28dfffa5ae6a3c58b413f
User <User@K3-512-04>
2017-12-06 19:25:16 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5, b = 1;
+
+            
+            Swap(ref a, ref b);
+
+            Console.WriteLine("a={0},b={1}", a, b);
+        }
+
+        /// <summary>
+        /// Обмен значений переменных
+        /// </summary>
+        /// <param name="a">Первая переменная</param>
+        /// <param name="b">Вторая переменная</param>
+
+
+        private static void Swap(ref int a, ref int b)
+        {
+            int c = a;
+            a = b;
+            b = c;
+        }
+
+       
+    }
+}
